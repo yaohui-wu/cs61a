6dc7e49a1a1a4ce839b0fcd7e40f86b2bcdd3b3a
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-02-28 21:06:23 -0500

    Update Project 1: The Game of Hog

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -344,7 +344,10 @@ def boar_strategy(score, opponent_score, threshold=11, num_rolls=6):
     points, and returns NUM_ROLLS otherwise. Ignore score and Sus Fuss.
     """
     # BEGIN PROBLEM 10
-    return num_rolls  # Remove this line once implemented.
+    points = boar_brawl(score, opponent_score)
+    if points >= threshold:
+        return 0
+    return num_rolls
     # END PROBLEM 10
 
 
