5eb335c19396b3c9c9aeda26992c9bdd85900305
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-03-11 22:23:19 -0400

    Update Lab 5: Mutability, Iterators

--- labs/lab05/lab05.py
+++ labs/lab05/lab05.py
@@ -27,6 +27,13 @@ def insert_items(s, before, after):
     True
     """
     "*** YOUR CODE HERE ***"
+    i = 0
+    while i < len(s):
+        if s[i] == before:
+            s.insert(i + 1, after)
+            i += 1
+        i += 1
+    return s
 
 
 def group_by(s, fn):
