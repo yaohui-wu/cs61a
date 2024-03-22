3e4823bf7659e3b4469331c6c3c5acaf5b16044b
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-03-21 21:39:30 -0400

    Update Project 3: Ants Vs. SomeBees

--- projects/ants/ants.py
+++ projects/ants/ants.py
@@ -27,6 +27,8 @@ class Place:
         # Phase 1: Add an entrance to the exit
         # BEGIN Problem 2
         "*** YOUR CODE HERE ***"
+        if self.exit:
+            self.exit.entrance = self
         # END Problem 2
 
     def add_insect(self, insect):
