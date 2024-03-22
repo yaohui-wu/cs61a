5c645d6ddaac6a53ccf2b7993cc138fc308a2f60
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-03-22 13:18:47 -0400

    Update Project 3: Ants Vs. SomeBees

--- projects/ants/ants.py
+++ projects/ants/ants.py
@@ -119,7 +119,14 @@ class Ant(Insect):
             place.ant = self
         else:
             # BEGIN Problem 8b
-            assert place.ant is None, 'Too many ants in {0}'.format(place)
+            other = place.ant
+            if other.can_contain(self):
+                other.store_ant(self)
+            elif self.can_contain(other):
+                self.store_ant(other)
+                place.ant = self
+            else:
+                assert place.ant is None, 'Too many ants in {0}'.format(place)
             # END Problem 8b
         Insect.add_to(self, place)
 
