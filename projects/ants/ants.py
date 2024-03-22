78ded3ead44970da0df4a7ae0b9b9947b07b6e8b
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-03-22 13:36:19 -0400

    Update Project 3: Ants Vs. SomeBees

--- projects/ants/ants.py
+++ projects/ants/ants.py
@@ -373,6 +373,19 @@ class BodyguardAnt(ContainerAnt):
 
 # BEGIN Problem 9
 # The TankAnt class
+class TankAnt(ContainerAnt):
+    name = 'Tank'
+    implemented = True
+    food_cost = 6
+    damage = 1
+
+    def __init__(self, health=2):
+        super().__init__(health)
+    
+    def action(self, gamestate):
+        for bee in self.place.bees[:]:
+            bee.reduce_health(self.damage)
+        super().action(gamestate)
 # END Problem 9
 
 
