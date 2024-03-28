09ae672644df66b73fc3cb09f58c70690d690361
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-03-27 21:10:20 -0400

    Update Homework 5: Generators

--- homeworks/hw05/hw05.py
+++ homeworks/hw05/hw05.py
@@ -79,10 +79,10 @@ def yield_paths(t, value):
     [[0, 2], [0, 2, 1, 2]]
     """
     if label(t) == value:
-        yield ____
+        yield [value]
     for b in branches(t):
-        for ____ in ____:
-            yield ____
+        for path in yield_paths(b, value):
+            yield [label(t)] + path
 
 
 
