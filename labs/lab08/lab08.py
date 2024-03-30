86446d4239cb06b6b55ce08ac282ad7c861b91a7
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-03-29 22:33:20 -0400

    Update Lab 8: Mutable Trees

--- labs/lab08/lab08.py
+++ labs/lab08/lab08.py
@@ -12,6 +12,9 @@ def cumulative_mul(t):
     Tree(5040, [Tree(60, [Tree(3), Tree(4), Tree(5)]), Tree(42, [Tree(7)])])
     """
     "*** YOUR CODE HERE ***"
+    for b in t.branches:
+        cumulative_mul(b)
+        t.label *= b.label
 
 
 def prune_small(t, n):
