2f6393e5e78107c8b86f353f7f59e140c078d0eb
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-03-29 22:36:59 -0400

    Update Lab 8: Mutable Trees

--- labs/lab08/lab08.py
+++ labs/lab08/lab08.py
@@ -34,11 +34,11 @@ def prune_small(t, n):
     >>> t3
     Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2)])])
     """
-    while ___________________________:
-        largest = max(_______________, key=____________________)
-        _________________________
-    for __ in _____________:
-        ___________________
+    while len(t.branches) > n:
+        largest = max(t.branches, key=lambda x: x.label)
+        t.branches.remove(largest)
+    for b in t.branches:
+        prune_small(b, n)
 
 
 def delete(t, x):
