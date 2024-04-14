a6554878466e7b5c21743786bc32cfba2f60c992
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-04-14 18:17:50 -0400

    Update Lab 9: Scheme

--- labs/lab09/lab09.scm
+++ labs/lab09/lab09.scm
@@ -1,4 +1,9 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+    (cond
+        ((< num1 num2) -1)
+        ((= num1 num2) 0)
+        (else 1))
+)
 
 (define (make-adder num) 'YOUR-CODE-HERE)
 
@@ -8,12 +13,12 @@
 
 (define (max a b)
   (if (> a b)
-      a
-      b))
+        a
+        b))
 
 (define (min a b)
   (if (> a b)
-      b
-      a))
+        b
+        a))
 
 (define (gcd a b) 'YOUR-CODE-HERE)
