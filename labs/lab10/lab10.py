dee6fd1e907b5fe8e33a01bb575a69be1008a63e
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-04-20 19:28:44 -0400

    Update Lab 10: Interpreters

--- labs/lab10/lab10.py
+++ labs/lab10/lab10.py
@@ -26,8 +26,8 @@ def calc_eval(exp):
         return OPERATORS[exp]
     elif isinstance(exp, int) or isinstance(exp, bool):   # Numbers and booleans
         return exp
-    elif _________________: # CHANGE THIS CONDITION FOR Q4
-        return _________________ # UPDATE THIS FOR Q4
+    elif exp in bindings: # CHANGE THIS CONDITION FOR Q4
+        return bindings[exp] # UPDATE THIS FOR Q4
 
 def calc_apply(op, args):
     return op(args)
@@ -108,6 +108,10 @@ def eval_define(expressions):
     2
     """
     "*** YOUR CODE HERE ***"
+    symbol = expressions.first
+    value = calc_eval(expressions.rest.first)
+    bindings[symbol] = value
+    return symbol
 
 OPERATORS = { "//": floor_div, "+": addition, "-": subtraction, "*": multiplication, "/": division }
 
