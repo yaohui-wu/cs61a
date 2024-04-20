cff36272babe61b2b1e4869abdde78474410f1e0
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-04-20 19:48:41 -0400

    Update Homework 8: Scheme Lists

--- homeworks/hw08/hw08.scm
+++ homeworks/hw08/hw08.scm
@@ -10,6 +10,10 @@
         (else (my-filter pred (cdr s))))
 )
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+    (cond ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1) (interleave lst2 (cdr lst1)))))
+)
 
 (define (no-repeats s) 'YOUR-CODE-HERE)
