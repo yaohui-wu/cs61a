2db6485c8f81f66e99270102eaeaeb8ef089ca4b
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-04-20 19:41:39 -0400

    Update Homework 8: Scheme Lists

--- homeworks/hw08/hw08.scm
+++ homeworks/hw08/hw08.scm
@@ -4,7 +4,11 @@
         (and (<= (car s) (car (cdr s))) (ascending? (cdr s))))
 )
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+    (cond ((null? s) '())
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s))))
+)
 
 (define (interleave lst1 lst2) 'YOUR-CODE-HERE)
 
