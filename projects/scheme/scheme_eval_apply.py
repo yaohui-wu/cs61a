5a612f55bac8edc7ff067c67d05158317ee85f33
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-04-26 21:06:07 -0400

    Update Project 4: Scheme Interpreter

--- projects/scheme/scheme_eval_apply.py
+++ projects/scheme/scheme_eval_apply.py
@@ -45,10 +45,18 @@ def scheme_apply(procedure, args, env):
     if isinstance(procedure, BuiltinProcedure):
         # BEGIN PROBLEM 2
         "*** YOUR CODE HERE ***"
+        python_args = []
+        curr = args
+        while curr is not nil:
+            python_args.append(curr.first)
+            curr = curr.rest
+        if procedure.need_env:
+            python_args.append(env)
         # END PROBLEM 2
         try:
             # BEGIN PROBLEM 2
             "*** YOUR CODE HERE ***"
+            return procedure.py_func(*python_args)
             # END PROBLEM 2
         except TypeError as err:
             raise SchemeError('incorrect number of arguments: {0}'.format(procedure))
