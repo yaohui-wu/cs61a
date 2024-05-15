c14b8091499edf01e8032644fc94e9562dd9776e
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-05-14 21:30:37 -0400

    Update Homework 9: Programs as Data, Macros

--- homeworks/hw09/hw09.scm
+++ homeworks/hw09/hw09.scm
@@ -4,7 +4,9 @@
     `(lambda (,(car formals)) ,(curry-cook (cdr formals) body))))
 
 (define (curry-consume curry args)
-  'YOUR-CODE-HERE)
+  (if (null? args)
+    curry
+    (curry-consume (curry (car args)) (cdr args))))
 
 (define-macro (switch expr options)
   (switch-to-cond (list 'switch expr options)))
